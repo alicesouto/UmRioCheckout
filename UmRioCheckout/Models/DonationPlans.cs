2e1d72c80f7c66d82354835bea0d4c4b8a2d3572
Alice Souto <alicesouto@poli.ufrj.br>
2016-10-19 17:59:15 -0200

    Update Plans

--- UmRioCheckout/Models/DonationPlans.cs
+++ UmRioCheckout/Models/DonationPlans.cs
@@ -12,7 +12,7 @@ namespace UmRioCheckout.Models
         public DonationPlans()
         {
             Amount = new List<double>();
-            Amount.Add(10.00);
+            Amount.Add(15.00);
             Amount.Add(30.00);
         }
         
